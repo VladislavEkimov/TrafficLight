e5d3bca0f3de137ac89e314179e384020d7caa4f
vladislav <vladis27449@yandex.ru>
2026-01-12 21:38:43 +0700

    Add view

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var actionButton: UIButton!
+    
+    private let offLight: CGFloat = 0.2
+    private let onLight: CGFloat = 1
+    private var currentLight: Light = .green
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = offLight
+        greenView.alpha = offLight
+        yellowView.alpha = offLight
+        
+        redView.layer.cornerRadius = redView.layer.frame.height / 2
+        yellowView.layer.cornerRadius = yellowView.layer.frame.height / 2
+        greenView.layer.cornerRadius = greenView.layer.frame.height / 2
     }
-
-
+    
+    @IBAction func actionLight(_ sender: Any) {
+        actionButton.setTitle("Next", for: .normal)
+        switch currentLight {
+        case .red:
+            currentLight = .yellow
+            redView.alpha = offLight
+            yellowView.alpha = onLight
+        case .yellow:
+            currentLight = .green
+            yellowView.alpha = offLight
+            greenView.alpha = onLight
+        case .green:
+            currentLight = .red
+            greenView.alpha = offLight
+            redView.alpha = onLight
+        }
+    }
+    
 }
 
+// MARK: - Light
+extension ViewController {
+    private enum Light {
+        case red
+        case yellow
+        case green
+    }
+}
